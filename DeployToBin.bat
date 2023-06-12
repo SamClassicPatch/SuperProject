11e47feb8fabc53d722d331ed60e64e6624f00c0
Dreamy Cecil <stalker27production@gmail.com>
2023-06-12 18:37:32 +0300

    Add description and thumbnail files for the mod.

--- DeployToBin.bat
+++ DeployToBin.bat
@@ -1,7 +1,9 @@
-@echo off
-echo Copying TSE 1.07 binaries...
-echo ----------------------------
+:: This script will copy built binaries of the patch for TSE 1.07 into the game's Bin directory
+:: one level above this one, including a GRO package with all the needed resources packed
 
+@echo off
+
+:: Copy TSE 1.07 binaries
 echo ClassicsCore
 copy /B Bin\Release_TSE107\ClassicsCore.dll ..\Bin\
 
@@ -21,12 +23,9 @@ echo Shaders
 copy /B Bin\Release_TSE107\Shaders.dll ..\Bin\
 
 echo EngineGUI
-copy /B /-Y Bin\Release_TSE107\EngineGUI.dll ..\Bin\
+copy /B Bin\Release_TSE107\EngineGUI.dll ..\Bin\
 
-echo ----------------------------
-echo Copying mod binaries...
-echo ----------------------------
-
+:: Copy mod files
 echo Entities
 copy /B Bin\Release_TSE107\Entities_Custom.dll ..\Bin\
 
@@ -36,11 +35,14 @@ copy /B Bin\Release_TSE107\Game_Custom.dll ..\Bin\
 echo GameGUI
 copy /B Bin\Release_TSE107\GameGUI_Custom.dll ..\Bin\
 
-echo ----------------------------
-echo Copying TSE 1.07 plugins...
-echo ----------------------------
+echo ClassicsPatchMod
+robocopy Builds\TSE107\Mods ..\Mods\ /E
 
+:: Copy TSE 1.07 plugins
 copy /B Bin\Release_TSE107\Plugins\*.dll ..\Bin\Plugins\
 
-echo ----------------------------
+:: Pack resources
+call Resources\DeployResources.bat %~dp0..\
+
+echo.
 echo Finished deploying!
