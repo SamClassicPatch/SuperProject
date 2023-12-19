59bd4cf7c64b03bbdf6517601b63cb2ae25f3709
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2023-12-19 16:56:25 +0300

    Update build script.

--- Builds/AssembleBuilds.bat
+++ Builds/AssembleBuilds.bat
@@ -14,6 +14,7 @@ echo ----------------------------
 cd %REPO%
 
 :: Collect files for the TFE 1.05 build
+copy /B %BUILDS%steam_api.dll                          %BUILDS%TFE105\Bin\
 copy /B Bin\Release_TFE105\ClassicsCore.dll            %BUILDS%TFE105\Bin\
 copy /B Bin\Release_TFE105\ClassicsPatches.dll         %BUILDS%TFE105\Bin\
 copy /B Bin\Release_TFE105\DedicatedServer_Custom.exe  %BUILDS%TFE105\Bin\
@@ -35,6 +36,7 @@ echo ----------------------------
 cd %REPO%
 
 :: Collect files for the TSE 1.05 build
+copy /B %BUILDS%steam_api.dll                          %BUILDS%TSE105\Bin\
 copy /B Bin\Release_TSE105\ClassicsCore.dll            %BUILDS%TSE105\Bin\
 copy /B Bin\Release_TSE105\ClassicsPatches.dll         %BUILDS%TSE105\Bin\
 copy /B Bin\Release_TSE105\DedicatedServer_Custom.exe  %BUILDS%TSE105\Bin\
@@ -56,6 +58,7 @@ echo ----------------------------
 cd %REPO%
 
 :: Collect files for the TSE 1.07 build
+copy /B %BUILDS%steam_api.dll                          %BUILDS%TSE107\Bin\
 copy /B Bin\Release_TSE107\ClassicsCore.dll            %BUILDS%TSE107\Bin\
 copy /B Bin\Release_TSE107\ClassicsPatches.dll         %BUILDS%TSE107\Bin\
 copy /B Bin\Release_TSE107\DedicatedServer_Custom.exe  %BUILDS%TSE107\Bin\
