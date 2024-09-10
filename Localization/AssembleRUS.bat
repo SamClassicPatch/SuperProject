9f2ea1dd8fe93aa759561196d1654d425b5f6d49
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-09-10 14:44:51 +0300

    Add tools for creating new localizations and updating existing ones.

--- Localization/AssembleRUS.bat
+++ Localization/AssembleRUS.bat
@@ -1,12 +1,7 @@
-:: You need to specify 7-Zip installation directory in the PATH environment variable for this script to work!
-
-:: This script creates a GRO package with Russian localization
-
 @echo off
 
-:: Remove old package
 cd %~dp0
-del SE1_ClassicsPatch_RUS.gro
+del SE1_ClassicsPatch_RUS.gro /q
 
 cd RUS\
 7z.exe a -tzip ..\SE1_ClassicsPatch_RUS.gro *
