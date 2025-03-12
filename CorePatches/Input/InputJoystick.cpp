5635f82cd53867adc9bb3ccf4bcad1535ea401e5
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2025-03-12 16:16:40 +0300

    Update copyright years in license headers.

--- CorePatches/Input/InputJoystick.cpp
+++ CorePatches/Input/InputJoystick.cpp
@@ -1,5 +1,5 @@
 /* Copyright (c) 2002-2012 Croteam Ltd.
-   Copyright (c) 2024 Dreamy Cecil
+   Copyright (c) 2024-2025 Dreamy Cecil
 This program is free software; you can redistribute it and/or modify
 it under the terms of version 2 of the GNU General Public License as published by
 the Free Software Foundation
