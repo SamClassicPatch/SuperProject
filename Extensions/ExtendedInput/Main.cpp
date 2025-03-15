86f0bdbfa3b98c37763dfee7f1a674980399aeab
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2025-03-15 14:42:57 +0300

    Add project files for ExtendedInput extension.

---
+++ Extensions/ExtendedInput/Main.cpp
@@ -0,0 +1,31 @@
+/* Copyright (c) 2022-2025 Dreamy Cecil
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#include "StdH.h"
+
+// Define extension
+CLASSICSPATCH_DEFINE_EXTENSION("PATCH_EXT_input", k_EPluginFlagGame | k_EPluginFlagEditor, CORE_PATCH_VERSION,
+  "Dreamy Cecil", "Extended Input",
+  "Extended input functionality that includes proper support of game controllers.");
+
+// Module entry point
+CLASSICSPATCH_PLUGIN_STARTUP(HIniConfig props, PluginEvents_t &events)
+{
+};
+
+// Module cleanup
+CLASSICSPATCH_PLUGIN_SHUTDOWN(HIniConfig props)
+{
+};
