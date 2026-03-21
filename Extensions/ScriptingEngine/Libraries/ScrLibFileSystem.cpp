427644a5c193d7843f369d673c78b330faa96469
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2026-03-22 00:52:38 +0300

    Implement CFontData as a Squirrel class in the FileSystem library.

--- Extensions/ScriptingEngine/Libraries/ScrLibFileSystem.cpp
+++ Extensions/ScriptingEngine/Libraries/ScrLibFileSystem.cpp
@@ -17,12 +17,75 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 namespace sq {
 
+// CFontData class methods
+namespace SqFontData {
+
+static SQInteger Constructor(HSQUIRRELVM v, int ctArgs, CFontData &val) {
+  const SQChar *strPath;
+
+  if (ctArgs == 0 || SQ_FAILED(sq_getstring(v, 2, &strPath))) {
+    return sq_throwerror(v, "expected a path to a font in argument 1");
+  }
+
+  try {
+    val.Load_t(CTString(strPath));
+  } catch(char *strError) {
+    return sq_throwerror(v, strError);
+  }
+
+  return 0;
+};
+
+SQCLASS_GETSET_INT(GetCharSpacing, SetCharSpacing, CFontData, val.fd_pixCharSpacing, val.fd_pixCharSpacing);
+SQCLASS_GETSET_INT(GetLineSpacing, SetLineSpacing, CFontData, val.fd_pixLineSpacing, val.fd_pixLineSpacing);
+SQCLASS_GETSET_BOOL(GetFixedWidth, SetFixedWidth, CFontData, val.fd_bFixedWidth, val.fd_bFixedWidth);
+
+static SQInteger GetTextureFile(HSQUIRRELVM v, int, CFontData &val) {
+  sq_pushstring(v, val.fd_fnTexture.str_String, -1);
+  return 1;
+};
+
+static SQInteger GetWidth(HSQUIRRELVM v, int, CFontData &val) {
+  sq_pushinteger(v, val.GetWidth());
+  return 1;
+};
+
+static SQInteger GetHeight(HSQUIRRELVM v, int, CFontData &val) {
+  sq_pushinteger(v, val.GetHeight());
+  return 1;
+};
+
+static Method<CFontData> _aMethods[] = {
+  { "GetTextureFile", &GetTextureFile, 1, "." },
+  { "GetWidth",       &GetWidth,       1, "." },
+  { "GetHeight",      &GetHeight,      1, "." },
+};
+
+}; // namespace
+
 namespace FileSystem {
 
 }; // namespace
 
 void VM::RegisterFileSystem(void) {
+  Table sqtFileSystem = Root().RegisterTable("FileSystem");
+  INDEX i;
 
+  // Register classes
+  {
+    Class<CFontData> sqcFont(GetVM(), "CFontData", &SqFontData::Constructor);
+
+    // Methods
+    for (i = 0; i < ARRAYCOUNT(SqFontData::_aMethods); i++) {
+      sqcFont.RegisterMethod(SqFontData::_aMethods[i]);
+    }
+
+    sqcFont.RegisterVar("pixCharSpacing", &SqFontData::GetCharSpacing, &SqFontData::SetCharSpacing);
+    sqcFont.RegisterVar("pixLineSpacing", &SqFontData::GetLineSpacing, &SqFontData::SetLineSpacing);
+    sqcFont.RegisterVar("bFixedWidth",    &SqFontData::GetFixedWidth,  &SqFontData::SetFixedWidth);
+
+    Root().AddClass(sqcFont);
+  }
 };
 
 }; // namespace
